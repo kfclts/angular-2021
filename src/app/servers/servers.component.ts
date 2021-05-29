7f971caf3318317eb2bdfab9a5828fc4e6874ef1
KFC <kfclts@gmail.com>
2021-05-30 02:33:29 +0800

    23. Fully Understanding the Component Selector

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // Angular does not support id selector or pseudo selector like hover also doesn't support
   selector: 'app-servers',
+  // selector: '[app-servers]', // attribute selector
+  // selector: '.app-servers', // class selector
   template: `<app-server></app-server><p>22</p><app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
