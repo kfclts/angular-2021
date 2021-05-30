196f6e23a1afd66f0bdf7dda28fba3c7e13ee4c8
KFC <kfclts@gmail.com>
2021-05-30 16:07:36 +0800

    26. String Interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
 })
 
 export class ServerComponent {
+  serverId: number = 10
+  serverStatus: string = 'offline'
 
+  getServerStatus() {
+    return this.serverStatus
+  }
+
+  getServerStatusArrow = () => {
+    return this.serverStatus
+  }
 }
