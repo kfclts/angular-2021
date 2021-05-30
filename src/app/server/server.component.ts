1490096e372c4d6bf4d6505e75b90c8eaf7af2ca
KFC <kfclts@gmail.com>
2021-05-30 20:28:17 +0800

    39. Styling Elements Dynamically with ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,11 +9,19 @@ export class ServerComponent {
   serverId: number = 10
   serverStatus: string = 'offline'
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline'
+  }
+
   getServerStatus() {
     return this.serverStatus
   }
 
   getServerStatusArrow = () => {
     return this.serverStatus
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red'
   }
 }
